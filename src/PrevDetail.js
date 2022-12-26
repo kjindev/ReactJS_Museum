fc04492de3e82b44698f4e828dc61ac050c5c89e
kjin <pkjin.eng@gmail.com>
2022-12-26 23:08:51 +0900

    221226

--- src/PrevDetail.js
+++ src/PrevDetail.js
@@ -1,4 +1,10 @@
-import React, { useState, useLayoutEffect, useRef, useEffect } from "react";
+import React, {
+  useState,
+  useLayoutEffect,
+  useRef,
+  useEffect,
+  useSyncExternalStore,
+} from "react";
 import { useLocation } from "react-router-dom";
 
 export default function PrevDetail({ dataPrev }) {
@@ -6,20 +12,17 @@ export default function PrevDetail({ dataPrev }) {
   const [submitText, setSubmitText] = useState("");
   const [dataLoading, setdataLoading] = useState(true);
   const monthList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  let start = [2021, 1];
-  let end = [2023, 12];
+  const [start, setStart] = useState([2021, 1]);
+  const [end, setEnd] = useState([2023, 12]);
 
   const { pathname } = useLocation();
 
-  const nameRef = useRef([]);
+  const dataRef = useRef([]);
   const dataArr = [];
   for (let i = 0; i < 8; i++) {
     dataArr[i] = dataPrev[i];
   }
-  /*
-  const io = new IntersectionObserver(콜백함수, { threshold: 0.7 });
-  const targetRef = useRef();
-*/
+
   useEffect(() => {
     setdataLoading(false);
   }, [pathname]);
@@ -28,10 +31,37 @@ export default function PrevDetail({ dataPrev }) {
     window.scroll(0, 0);
   }, []);
 
-  const handleTextSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     setSubmitText(searchText);
-    setSearchText("");
+    for (let i = 0; i < dataPrev.length; i++) {
+      dataRef.current.children[i].classList.remove("hidden");
+      if (
+        dataRef.current.children[i].innerText.includes(submitText) === false &&
+        dataRef.current.children[i].children[2].innerText.includes(
+          submitText
+        ) === false
+      ) {
+        dataRef.current.children[i].classList.add("hidden");
+      } else if (submitText === "") {
+        dataRef.current.children[i].classList.remove("hidden");
+      }
+    }
+  };
+
+  const handleClick = () => {
+    for (let i = 0; i < dataPrev.length; i++) {
+      const startDate = dataRef.current.children[i].children[3].innerText;
+      const endDate = dataRef.current.children[i].children[4].innerText;
+      dataRef.current.children[i].classList.remove("hidden");
+      console.log("2022-11-30" > `${start[0]}-${start[1]}-1`);
+      if (
+        startDate < `${start[0]}-${start[1]}-1` ||
+        endDate > `${end[0]}-${end[1]}-31`
+      ) {
+        dataRef.current.children[i].classList.add("hidden");
+      }
+    }
   };
 
   const handleTextChange = (event) => {
@@ -40,17 +70,17 @@ export default function PrevDetail({ dataPrev }) {
 
   const handleStartClick = (event) => {
     if (event.target.name === "year") {
-      start[0] = parseInt(event.target.value);
+      setStart([parseInt(event.target.value), start[1]]);
     } else if (event.target.name === "month") {
-      start[1] = parseInt(event.target.value);
+      setStart([start[0], parseInt(event.target.value)]);
     }
   };
 
   const handleEndClick = (event) => {
     if (event.target.name === "year") {
-      end[0] = parseInt(event.target.value);
+      setEnd([parseInt(event.target.value), end[1]]);
     } else if (event.target.name === "month") {
-      end[1] = parseInt(event.target.value);
+      setEnd([end[0], parseInt(event.target.value)]);
     }
   };
 
@@ -60,8 +90,8 @@ export default function PrevDetail({ dataPrev }) {
         <div className="w-2 h-12 bg-black mr-5"></div>
         <div className="text-5xl">지난 전시</div>
       </div>
-      <form onSubmit={handleTextSubmit} className="p-3">
-        <span>이름으로 검색하기</span>
+      <form onSubmit={handleSubmit} className="p-3">
+        <span>이름</span>
         <input
           placeholder="작품 및 아티스트 이름으로 검색해보세요"
           type="text"
@@ -69,15 +99,9 @@ export default function PrevDetail({ dataPrev }) {
           onChange={handleTextChange}
           className="w-[30%] drop-shadow-md p-2 px-3 mx-5"
         />
-        <button
-          type="submit"
-          className="w-[5%] bg-black drop-shadow-md text-white p-2"
-        >
-          검색
-        </button>
       </form>
       <div className="p-3">
-        <span>기간으로 검색하기</span>
+        <span>기간</span>
         <span className="pl-5" onClick={handleStartClick}>
           <span>전시 시작일</span>
           <select
@@ -123,16 +147,14 @@ export default function PrevDetail({ dataPrev }) {
           </select>
           <span>월</span>
         </span>
-        <button
-          type="submit"
-          className="w-[5%] bg-black drop-shadow-md text-white p-2"
-        >
+        <button onClick={handleClick} type="submit">
           검색
         </button>
       </div>
+
       <div
         className="w-[100%] h-[100%] flex flex-wrap justify-center content-start"
-        ref={nameRef}
+        ref={dataRef}
       >
         {dataLoading ? (
           <div>Loading...</div>
@@ -144,9 +166,12 @@ export default function PrevDetail({ dataPrev }) {
             >
               <img
                 src={item.DP_MAIN_IMG}
-                className="w-[100%] h-[100%] p-2 object-cover bg-white"
+                className="w-[100%] h-[80%] p-2 object-cover bg-white"
               />
               <div className="p-2">{item.DP_NAME}</div>
+              <div className="hidden">{item.DP_ARTIST}</div>
+              <div className="hidden">{item.DP_START}</div>
+              <div className="hidden">{item.DP_END}</div>
             </div>
           ))
         )}
